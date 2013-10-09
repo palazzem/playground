03e02c3c36808bcedac2c43fc5ed9d7cea9b04e5
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-10-09 21:14:18 +0200

    Added some fixtures

--- getting-started/js/application.js
+++ getting-started/js/application.js
@@ -1,3 +1,5 @@
 // Initialize an Ember application
 window.Todos = Ember.Application.create();
 
+// Adapter for fixtures
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
--- getting-started/js/models/todo.js
+++ getting-started/js/models/todo.js
@@ -1,4 +1,24 @@
+// Model definition
 Todos.Todo = DS.Model.extend({
     title: DS.attr('string'),
     isCompleted: DS.attr('boolean')
 });
+
+// Some fixtures
+Todos.Todo.FIXTURES= [
+    {
+        id: 1,
+        title: 'Watch the office series',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        title: 'Kill the giant spider :O',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        title: 'Release the Kraken!',
+        isCompleted: false
+    }
+];
