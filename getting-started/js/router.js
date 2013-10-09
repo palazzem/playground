ed052c5e16fc29166217d6fa7237a46dd2f6aab9
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-10-09 21:44:59 +0200

    Bind Ember model to handlebar template

--- getting-started/js/router.js
+++ getting-started/js/router.js
@@ -2,3 +2,10 @@
 Todos.Router.map(function () {
     this.resource('todos', { path: '/'});
 });
+
+// Router to display model data
+Todos.TodosRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.find('todo');
+    }
+});
