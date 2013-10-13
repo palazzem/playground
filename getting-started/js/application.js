0fe74d0a8e0e9f659c08d9e138a1096881c412c8
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-10-13 16:00:32 +0200

    Replace DS fixtures with local storage adapter. Ready to go offline

--- getting-started/js/application.js
+++ getting-started/js/application.js
@@ -1,5 +1,9 @@
 // Initialize an Ember application
 window.Todos = Ember.Application.create();
 
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
+});
+
 // Adapter for fixtures
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+// Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
