d77e5f85b650d0debfd5861a3f7e38c89d2ca3f4
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-10-13 15:46:43 +0200

    Filtering properties to remove all completed todo

--- getting-started/js/controllers/todo_controller.js
+++ getting-started/js/controllers/todo_controller.js
@@ -15,12 +15,25 @@ Todos.TodosController = Ember.ArrayController.extend({
             todo.save();
 
             this.set('newTitle', '');
+        },
+        clearCompleted: function () {
+            var completed = this.filterProperty('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
         }
     },
     
     remaining: function () {
         return this.filterBy('isCompleted', false).get('length');
     }.property('@each.isCompleted'),
+
+    completed: function () {
+        return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+
+    hasCompleted: function () {
+        return this.get('completed') > 0;
+    }.property('completed'),
 
     inflection: function () {
         var remaining = this.get('remaining');
