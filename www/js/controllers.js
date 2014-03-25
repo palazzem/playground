a525dfac083d9db632239c37770257e8edd7f11b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-03-25 23:41:16 +0100

    Create new task done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,12 +3,44 @@
 
 var todoApp = angular.module('todo');
 
-todoApp.controller('TodoCtrl', function($scope) {
+todoApp.controller('TodoCtrl', function($scope, $ionicModal) {
   $scope.tasks = [
     { title: 'Coding' },
     { title: 'Watch Walking Dead' },
     { title: 'Play with Starbound' },
     { title: 'Get milk' },
     { title: 'Finish read Secrets and Lies book (Schneier)' }
-  ]
+  ];
+
+  // Empty object
+  $scope.task = {};
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('views/modal.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    if (typeof task.title !== 'undefined' && task.title) {
+      $scope.tasks.push({
+        title: task.title
+      });
+      $scope.taskModal.hide();
+      task.title = '';
+    }
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 });
