7a0ed1c120f49303358d944082f8d2be73a4658a
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-03-15 17:35:57 +0100

    adding and showing new friends with custom routes

---
+++ ember/borrowers/app/controllers/friends/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        save: function () {
+            var _this = this;
+            this.get("model").save().then(function (friend) {
+                _this.transitionToRoute('friends.show', friend);
+            })
+
+            return false;
+        },
+        cancel: function () {
+            this.transitionToRoute('friends');
+
+            return false;
+        }
+    }
+});
