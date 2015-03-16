1eef91454d8b7f4cca4a4273eb57ff548a57290a
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-03-16 16:00:21 +0100

    adding fontello to Brocfile

--- ember/borrowers/Brocfile.js
+++ ember/borrowers/Brocfile.js
@@ -7,4 +7,11 @@ var app = new EmberApp();
 // Additional libraries
 app.import('bower_components/picnic/releases/v2.min.css');
 
+app.import('vendor/fontello/fontello.css');
+app.import('vendor/fontello/font/fontello.ttf', { destDir: 'font' });
+app.import('vendor/fontello/font/fontello.eot', { destDir: 'font' });
+app.import('vendor/fontello/font/fontello.svg', { destDir: 'font' });
+app.import('vendor/fontello/font/fontello.woff', { destDir: 'font' });
+
 module.exports = app.toTree();
+
