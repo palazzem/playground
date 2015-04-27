58adbcc9bf480342b769a96d56d339482f34f84b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-04-27 21:49:05 +0200

    added more gestures

--- myo-wearable/HelloMyo/app/src/main/java/me/palazzem/hellomyo/DeviceListener.java
+++ myo-wearable/HelloMyo/app/src/main/java/me/palazzem/hellomyo/DeviceListener.java
@@ -3,8 +3,10 @@ package me.palazzem.hellomyo;
 import android.util.Log;
 
 import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
 import com.thalmic.myo.Myo;
 import com.thalmic.myo.Pose;
+import com.thalmic.myo.XDirection;
 
 public class DeviceListener extends AbstractDeviceListener {
     @Override
@@ -16,24 +18,47 @@ public class DeviceListener extends AbstractDeviceListener {
     @Override
     public void onPose(Myo myo, long timestamp, Pose pose) {
         super.onPose(myo, timestamp, pose);
+
+        // doesn't lock the Myo until an explicit action is done
+        // such as myo.lock()
+        myo.unlock(Myo.UnlockType.HOLD);
+
         switch (pose) {
+            case DOUBLE_TAP:
+                // double tap works as a locker/unlocker
+                myo.lock();
+                break;
+            case REST:
+                Log.d(Constants.TAG_LOG, "'cmon!");
+                break;
+            case FIST:
+                Log.d(Constants.TAG_LOG, "Fist!");
+                break;
+            case FINGERS_SPREAD:
+                Log.d(Constants.TAG_LOG, "Hand!");
+                break;
             case WAVE_IN:
                 Log.d(Constants.TAG_LOG, "Left!");
                 break;
             case WAVE_OUT:
                 Log.d(Constants.TAG_LOG, "Right!");
                 break;
+            case UNKNOWN:
+                Log.d(Constants.TAG_LOG, "Unrecognized gesture :(");
+                break;
         }
+    }
 
-        // doesn't lock the Myo until an explicit action is done
-        // such as myo.lock()
-        myo.unlock(Myo.UnlockType.HOLD);
+    @Override
+    public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+        super.onArmSync(myo, timestamp, arm, xDirection);
+        Log.d(Constants.TAG_LOG, "Sync completed!");
     }
 
     @Override
     public void onLock(Myo myo, long timestamp) {
         super.onLock(myo, timestamp);
-        Log.d(Constants.TAG_LOG, "Locked again!");
+        Log.d(Constants.TAG_LOG, "Locked!");
     }
 
     @Override
