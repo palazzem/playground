fb23e2e0a4396ae0e888532d4d8c074732ae9a08
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-04-27 14:34:44 +0200

    Myo activity for Myo initialization with a basic gestures recognition

--- myo-wearable/HelloMyo/app/src/main/java/me/palazzem/hellomyo/MainActivity.java
+++ myo-wearable/HelloMyo/app/src/main/java/me/palazzem/hellomyo/MainActivity.java
@@ -1,17 +1,37 @@
 package me.palazzem.hellomyo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.scanner.ScanActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Hub mHub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boolean status;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hub initialization (manages Myo instances)
+        mHub = Hub.getInstance();
+        status = mHub.init(this);
+
+        // checks Hub initialization (fails if the system doesn't support Bluetooth Low Energy)
+        if (!status) {
+            Log.e(Constants.TAG_LOG, "Hub initialization failed!");
+        }
+
+        // disabling data usage send
+        mHub.setSendUsageData(false);
+        mHub.addListener(new DeviceListener());
     }
 
 
@@ -30,10 +50,16 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_pairing) {
+            myoPairing();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void myoPairing() {
+        // Use ScanActivity.class for Myo pairing
+        Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+        startActivity(intent);
     }
 }
