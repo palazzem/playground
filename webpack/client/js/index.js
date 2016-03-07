35b1c9c634143a8461cfb777ff959b43c0300120
Emanuele <emanuele.palazzetti@gmail.com>
2016-03-07 23:58:00 +0100

    using in the index.js

--- webpack/client/js/index.js
+++ webpack/client/js/index.js
@@ -1,3 +1,14 @@
-import $ from 'jquery';
+if (document.querySelectorAll('a').length > 0) {
+    require.ensure([], () => {
+        const Button = require('./button').default;
+        const button = new Button('https://www.google.com');
+        button.render('a');
+    }, 'button');
+}
 
-$('body').html('Hello');
+if (document.querySelectorAll('h1').length > 0) {
+    require.ensure([], () => {
+        const Header = require('./header').default;
+        new Header().render('h1');
+    });
+}
