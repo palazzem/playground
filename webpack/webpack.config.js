22d81e7d4ec4bea4f9bd83ae974db1328e1f3c41
Emanuele <emanuele.palazzetti@gmail.com>
2016-03-08 00:56:03 +0100

    added some steps and plugins for production build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,22 +1,58 @@
 var path = require('path');
 var webpack = require('webpack');
+var CleanPlugin = require('clean-webpack-plugin');
+var production = process.env.NODE_ENV === 'production';
+
+var plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'main',
+        children: true,
+        minChunks: 2,
+    }),
+];
+
+// adding production plugins
+if (production) {
+    plugins = plugins.concat([
+        // clean the build folder
+        new CleanPlugin('builds'),
+        // looks and merges similar chunks, preventing duplication
+        new webpack.optimize.DedupePlugin(),
+        // optimizes how chunks are used within application
+        new webpack.optimize.OccurenceOrderPlugin(),
+        // prevents from creating chunks lesser than ~50kb
+        new webpack.optimize.MinChunkSizePlugin({
+            minChunkSize: 51200,
+        }),
+        // minifies the JavaScript code
+        new webpack.optimize.UglifyJsPlugin(),
+
+        // defines various variables that we can set to false in production to avoid
+        // code related to them from being compiled in our final bundle
+        // NOTE: the right usage depends on your libraries code
+        new webpack.DefinePlugin({
+            __SERVER__:      !production,
+            __DEVELOPMENT__: !production,
+            __DEVTOOLS__:    !production,
+            'process.env':   {
+                BABEL_ENV: JSON.stringify(process.env.NODE_ENV),
+            },
+        }),
+    ]);
+}
 
 module.exports = {
-    debug: true,
+    debug: !production,
     entry: './client/js',
     output: {
         path: 'builds',
-        filename: 'main.js',
+        // TODO: change using [name]-[hash].js
+        filename: production ? 'main.js' : 'main.js',
+        chunkFilename: '[name]-[chunkhash].js',
         publicPath: '/builds/',
     },
-    devtool: 'source-map',
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'main',
-            children: true,
-            minChunks: 2,
-        }),
-    ],
+    plugins: plugins,
+    devtool: production ? false : 'source-map',
     module: {
         loaders: [
             {
