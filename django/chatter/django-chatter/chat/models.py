f326d67ce6c2c3b0a160118831eb1cbf85851bde
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-04-03 16:23:17 +0200

    added Room and Message models

--- django/chatter/django-chatter/chat/models.py
+++ django/chatter/django-chatter/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    handle = models.TextField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+    room = models.ForeignKey(Room, related_name='messages')
+
+    def __str__(self):
+        return '[{timestamp}] {message}'.format({timestamp: self.timestamp.isoformat(), message: self.message})
