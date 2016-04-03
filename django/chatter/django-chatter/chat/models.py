8af845fa4b3fbbbc85c01757780e574d2bff0038
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-04-03 18:58:43 +0200

    more on Message model

--- django/chatter/django-chatter/chat/models.py
+++ django/chatter/django-chatter/chat/models.py
@@ -17,4 +17,22 @@ class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages')
 
     def __str__(self):
-        return '[{timestamp}] {message}'.format({timestamp: self.timestamp.isoformat(), message: self.message})
+        return '[{timestamp}] {message}'.format({timestamp: self.format_timestamp, message: self.message})
+
+    @property
+    def format_timestamp(self):
+        """
+        Format the timestamp in ISO format
+        """
+        return self.timestamp.isoformat()
+
+    def as_dict(self):
+        """
+        Creates a dictionary that represents this instance;
+        used to post back to clients the current stored message
+        """
+        return {
+            'handle': self.handle,
+            'message': self.message,
+            'timestamp': self.format_timestamp,
+        }
