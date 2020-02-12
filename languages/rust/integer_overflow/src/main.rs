e787e5467620baf824695656994e8b1e978eed53
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2020-02-12 23:06:27 +0100

    [rust] Add float representation

--- languages/rust/integer_overflow/src/main.rs
+++ languages/rust/integer_overflow/src/main.rs
@@ -1,5 +1,16 @@
+use std::io;
+
 fn main() {
-    let mut number: u8 = 255;
-    number += 1;
-    println!("value: {}", number);
+    // Integer check for overflow
+    println!("Insert an integer number:");
+    let mut number = String::new();
+    io::stdin().read_line(&mut number).expect("Darn!");
+    let number: u8 = match number.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+    println!("Double value: {}", number * 2);
+
+    // Float representation: 0.2 + 0.1 = 0.30000000000000004
+    println!("0.2 + 0.1 = {}", 0.2 + 0.1);
 }
